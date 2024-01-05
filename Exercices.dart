5b64ac639f5446e4c37ecf22e6fd7f620df57f6c
Mouhamedoune  Fall <MEDOUNEFALL126@GMAIL.COM>
2024-01-05 21:57:37 +0000

    Exercice 1 done

---
+++ Exercices.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+
+void main()
+{
+
+  // Exercise 1 Determinons l'age d'un utilisateur
+  print("Entrer votre age:");
+  int? age = int.parse(stdin.readLineSync()!);
+  int res=100-age;
+  print("Ton age est ${age}");
+  print("Il vous reste $res pour atteindre les 100ans");
+  // Exercise 1 Done
+
+
+  // Exercise 2
+
+
+
+}
